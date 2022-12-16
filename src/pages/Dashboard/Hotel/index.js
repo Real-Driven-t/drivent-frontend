5987abbfb37f40d77b77385c0464a3197c77daee
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-12-16 11:24:31 -0300

    feat: implement hotelApi && hook useHotel

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,31 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      <MsgError>
+        Sua modalidade de ingresso não inclui hospedagem <br /> Prossiga para a escolha de atividades
+      </MsgError>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
+
+const MsgError = styled.div`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  text-align: center;
+  font-size: 20px;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  font-weight: 400;
+  color: #8e8e8e;
+`;
---
+++ src/services/hotelApi.js
@@ -0,0 +1,9 @@
+import api from './api';
+
+export async function getHotels(token) {
+  const response = await api.get('/hotels', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
