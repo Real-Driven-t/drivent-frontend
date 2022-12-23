f4234fb85e8213d7f92931dfefc70924028b3a37
Aikenkov <Fabio.rod.silva.spqr@gmail.com>
2022-12-23 00:44:08 -0300

    chore: merge

--- src/components/SelectActivities/Locals..js
+++ src/components/SelectActivities/Locals..js
@@ -1,24 +1,20 @@
 import { useEffect, useState } from 'react';
 import useActivitiesByDay from '../../hooks/api/useActivitiesByDate';
 import useDaysWithActivity from '../../hooks/api/useDaysWithActiviy';
-import Activities from './Activities';
 import DaysContainer from './DaysContainer';
 
 export default function Locals() {
-  //const body = { day: date.toISOString() };
-  const [ selectedDay, setSelectedDay ] = useState('');
+  const [selectedDay, setSelectedDay] = useState('');
   const [activities, setActivities] = useState([]);
   const { getActivitiestWithPlace } = useActivitiesByDay();
   const { days } = useDaysWithActivity();
+
   useEffect(() => {
     const promisse = getActivitiestWithPlace(selectedDay);
-    promisse.then(p => { if(p) setActivities(p); });
+    promisse.then((p) => {
+      if (p) setActivities(p);
+    });
   }, [selectedDay]);
-  console.log(activities);
-  return(
-    <>
-      {/*activities.length !== 0 ? <DayButton activities={activities} /> : <>Deu ruim...</>*/}
-      {days ? <DaysContainer days={days} setSelectedDay={setSelectedDay} activities={activities}/> : ''}
-    </>
-  );
+
+  return <>{days ? <DaysContainer days={days} setSelectedDay={setSelectedDay} activities={activities} /> : ''}</>;
 }
