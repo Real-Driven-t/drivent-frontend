28147e5c2531b35b60cce4efddc02e59aa96fe6a
Enzo <enzo202p@gmail.com>
2022-12-23 16:21:25 -0300

    feat: logida de botoes dos dias

--- src/components/SelectActivities/DaysContainer.js
+++ src/components/SelectActivities/DaysContainer.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 import Day from './Day';
 import Activities from './Activities';
+import { useState } from 'react';
 
 export default function DaysContainer({ days, setSelectedDay, activities }) {
+  const [selected, setSelected] = useState(-1);
   return (
     <>
       <Container>
         {days.map((d, i) => (
-          <Day key={i} day={d.day} setSelectedDay={setSelectedDay} />
+          <Day key={i} day={d.day} id={i} setSelectedDay={setSelectedDay} selected={selected} setSelected={setSelected}/>
         ))}
       </Container>
       {activities[0] ? <Activities activities={activities} /> : ''}
